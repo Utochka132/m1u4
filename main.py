ca6d20d3a8947912d04872619278e3fe7a7c4a21
Utochka132 <127588363+Utochka132@users.noreply.github.com>
2023-11-20 08:19:38 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import telebot 
+from config import token
+
+from logic import Pokemon
+
+bot = telebot.TeleBot(token) 
+
+@bot.message_handler(commands=['go'])
+def start(message):
+    if message.from_user.username not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(message.from_user.username)
+        bot.send_message(message.chat.id, pokemon.info())
+        bot.send_photo(message.chat.id, pokemon.show_img())
+    else:
+        del Pokemon.pokemons.keys()[0]
+
+
+bot.infinity_polling(none_stop=True)
+
